590d324641cbfd20f35cfa3ac71fd4359e29deea
George Softas <george.softas@gmail.com>
2018-05-15 13:24:30 +0300

    Express Static Web Server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+// Define the requirements
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Define the port the web server will listen to
+app.set('port', 8080);
+
+// Use Express to serve the static assets
+app.use(express.static(path.join(__dirname, './static')));
+
+// Start the server 
+const server = app.listen(app.get('port'), function() {
+	console.log('The server is running on: ' + app.get('port'));
+});
